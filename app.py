201f6fa3e085325a64cb700ec5aeb5a6cb4d355e
husnamukhtar98 <huyu0001@stud.kea.dk>
2025-05-28 15:52:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('bookbord/')
+@app.route('/bookbord')
 def bookbord():
     return render_template('bookbord.html')
 
